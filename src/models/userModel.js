1c9e1f3160711c91005d54654346290a5cb6453e
harshitha-brs <153584944+harshitha-brs@users.noreply.github.com>
2024-08-27 12:03:50 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -1,24 +1,34 @@
 import mongoose from "mongoose";
 
-const userSchema = mongoose.Schema({
-  first_name: { type: String, default: null },
-  last_name: { type: String, default: null },
-  email: { type: String, unique: true },
-  password: { type: String },
-  languages: { type: Array },
-  categories: { type: Array },
-  saved_stories: { type: Array },
-  token: { type: String },
-  verified: { type: Boolean, default: false },
-  verifyToken: { type: String }, //verifyToken with link from webiste
-  verifyTokenExpires: Date,
-  verifyCode: { type: String }, //verifyCode from Mobile
-  verifyCodeExpires: Date,
-  resetPasswordToken: { type: String }, //resetPasswordToken with link from webiste
-  resetPasswordExpires: Date,
-  resetPasswordCode: { type: String }, //resetPasswordCode from Mobile
-  resetPasswordCodeExpires: Date,
-});
+const userSchema = mongoose.Schema(
+  {
+    first_name: { type: String, default: null },
+    last_name: { type: String, default: null },
+    email: { type: String, unique: true },
+    password: { type: String },
+    languages: { type: Array },
+    categories: { type: Array },
+    saved_stories: { type: Array },
+    token: { type: String },
+    verified: { type: Boolean, default: false },
+    verify_token: { type: String },
+    verify_token_expires: Date,
+    reset_password_token: { type: String },
+    reset_password_epxpires: Date,
+    status: { type: Boolean, default: true },
+    // mobile
+    otp: { type: String },
+    otp_expires_in: Date,
+    reset_password_otp: { type: String },
+    reset_password_otp_expires_in: Date,
+  },
+  {
+    timestamps: {
+      createdAt: "created_at",
+      updatedAt: "updated_at",
+    },
+  }
+);
 
 const User = mongoose.model("User", userSchema);
 
